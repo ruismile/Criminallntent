a2bae7a734a0911279062ebaeeb561dcf14beeea
HanRui <rui1490560045@163.com>
2017-05-22 22:57:49 +0800

    完成Crimee明细界面.

--- app/src/main/java/com/hanrui/android/criminallntent/Crime.java
+++ app/src/main/java/com/hanrui/android/criminallntent/Crime.java
@@ -1,5 +1,8 @@
 package com.hanrui.android.criminallntent;
 
+import android.database.DatabaseErrorHandler;
+
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -10,10 +13,13 @@ import java.util.UUID;
 //创建Crime实例，表示办公室陋习
 public class Crime {
     private UUID mId;//随机产生一个id，重复性是没有的
-    private String mTitle;
+    private String mTitle;//标题
+    private Date mDate;//日期
+    private boolean mSolved;//事情是否解决
     
     public Crime(){
         mId=UUID.randomUUID();
+        mDate=new Date();
     }
 
     public UUID getId() {
@@ -26,5 +32,21 @@ public class Crime {
 
     public void setTitle(String title) {
         mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
--- app/src/main/java/com/hanrui/android/criminallntent/CrimeFragment.java
+++ app/src/main/java/com/hanrui/android/criminallntent/CrimeFragment.java
@@ -7,7 +7,12 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+
+import java.nio.channels.NonWritableChannelException;
 
 /**
  * @author
@@ -19,6 +24,8 @@ public class CrimeFragment extends Fragment{
 
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,6 +51,18 @@ public class CrimeFragment extends Fragment{
             @Override
             public void afterTextChanged(Editable s) {
 
+            }
+        });
+        
+        mDateButton=(Button)v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+        
+        mSolvedCheckBox=(CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
             }
         });
         
